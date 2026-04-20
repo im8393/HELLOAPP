5e25312ee82e083196157fe0dd0eb0d73abda2da
im8393 <im8393@srmist.edu.in>
2026-04-20 22:24:25 +0530

    added UC7

--- helloapp.java
+++ helloapp.java
@@ -1,22 +1,9 @@
-public class helloapp {
+public class Helloapp {
     public static void main(String[] args) {
-        String name;
-
-        if (args.length == 0) {
-            name = "World";
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-            for (String n : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(n);
-                first = false;
-            }
-            name = nameBuilder.toString();
-        }
-
-        System.out.println("Hello, " + name + "!");
+       String names = "World";
+       if (args.length > 0) {
+           names = String.join(",",args);
+       }
+       System.out.println("Hello, + names +"!");
     }
 }
